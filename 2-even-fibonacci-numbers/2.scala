92d43786bf28e431135b648b8903c2fe885f95f5
Ilya Shaisultanov <ilya.shaisultanov@gmail.com>
2013-11-10 16:33:53 -0500

    Packaged Fibonacci generator.

--- 2-even-fibonacci-numbers/2.scala
+++ 2-even-fibonacci-numbers/2.scala
@@ -1,25 +1,7 @@
-var memo = scala.collection.mutable.HashMap[Int, Int]()
+// run from the root directory:
+// scala -classpath `pwd` 2-even-fibonacci-numbers/2.scala
 
-// Recursive with memo
-def fib(n: Int): Int = {
-  if (n == 0 || n == 1) n
-  else {
-    if (memo.getOrElse(n-2, -1) == -1) memo(n-2) = fib(n-2)
-    if (memo.getOrElse(n-1, -1) == -1) memo(n-1) = fib(n-1)
-
-    memo.get(n-2).get + memo.get(n-1).get
-  }
-}
-
-// Tail-recursive, no memo
-def fibTail(n: Int): Int = {
-  def _f(_n: Int, a: Int, b: Int): Int = _n match {
-    case `n` => a
-    case _ => _f(_n+1, b, a+b)
-  }
-
-  _f(1, 1, 1)
-}
+import fibonacci._
 
 def sumEvenFib(limit: Int): Int = {
   var sum = 0
@@ -29,7 +11,7 @@ def sumEvenFib(limit: Int): Int = {
   while (fSeq < limit) {
     if (fSeq % 2 == 0) sum = sum + fSeq
     i = i + 1
-    fSeq = fibTail(i)
+    fSeq = fibonacci.get(i)
   }
 
   sum
---
+++ fibonacci.scala
@@ -0,0 +1,29 @@
+package fibonacci
+
+object fibonacci {
+  // Tail-recursive, no memo
+  def get(n: Int): Int = {
+    def _f(_n: Int, a: Int, b: Int): Int = _n match {
+      case `n` => a
+      case _ => _f(_n+1, b, a+b)
+    }
+
+    _f(1, 1, 1)
+  }  
+}
+
+// Not used
+object fibonacci_rec {
+  var memo = scala.collection.mutable.HashMap[Int, Int]()
+
+  // Recursive with memo
+  def get(n: Int): Int = {
+    if (n == 0 || n == 1) n
+    else {
+      if (memo.getOrElse(n-2, -1) == -1) memo(n-2) = get(n-2)
+      if (memo.getOrElse(n-1, -1) == -1) memo(n-1) = get(n-1)
+
+      memo.get(n-2).get + memo.get(n-1).get
+    }
+  }
+}
