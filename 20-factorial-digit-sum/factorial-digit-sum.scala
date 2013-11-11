30910da30369b4b42a543291bd8594458007d760
Ilya Shaisultanov <ilya.shaisultanov@gmail.com>
2013-11-10 22:37:03 -0500

    Solved #20.

---
+++ 20-factorial-digit-sum/factorial-digit-sum.scala
@@ -0,0 +1,9 @@
+// scala -classpath `pwd` 20-factorial-digit-sum/factorial-digit-sum.scala
+
+import factorial._
+
+val sum = factorial.get(100).toString
+  .map((c) => c.toString.toInt)
+  .reduce(_+_)
+
+System.out.println(sum)
