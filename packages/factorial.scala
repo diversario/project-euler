117b063b3add3cc196e62fdfebf022c647fda8d6
Ilya Shaisultanov <ilya.shaisultanov@gmail.com>
2013-11-13 09:03:38 -0500

    Update factorial.scala
    Less code, less lame!

--- packages/factorial.scala
+++ packages/factorial.scala
@@ -4,13 +4,12 @@ object factorial {
 
   // Tail-recursive factorial calculator
   def get(n: Int): BigInt = {
-    def _f(_n: Int, z: BigInt): BigInt = _n match {
-      case 0 => 1
-      case `n` => z
-      case _ => _f(_n + 1, z * (_n + 1))
+    def _f(_n: Int, acc: BigInt): BigInt = _n match {
+      case 1 => acc
+      case _ => _f(_n - 1, acc * _n)
     }
 
-    _f(1, 1)
+    _f(n, 1)
   }
 
 }
